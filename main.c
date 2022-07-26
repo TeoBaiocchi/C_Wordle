f83862e8a6341e4651b246043af8e35a5b01409d
TeoBaiocchi <teobaiocchiensinck@gmail.com>
2022-07-26 20:54:24 -0300

    Añadidas funciones de color de output y continuada la estructura del programa

--- main.c
+++ main.c
@@ -2,18 +2,35 @@
 #include <stdlib.h>
 
 
-//Esta funcion simplemente limpia la consola, tanto en windows como en linux
+//Esta funcion simplemente limpia la consola, y detecta automaticamente el 
+//sistema operativo para cambiar el llamado al sistema que lo hace (multiplataforma)
 void limpiarPantalla();
 
-//Esta funcion supuestamente limpia el buffer. No te voy a mentir, no recuerdo como funciona o por qué hacia falta
-//Pero puede usarse como un "presiona enter para continuar"
+//Funcion "presione enter para continuar" / tambien limpia el buffer 
+//puede ser util en algunos casos de ingreso de strings 
+// (Revisar)
 void bufferEnter();
 
-//Esta bueno ir seccionando estas cosas en lugar de tener todo en el main
+//Las secciones del programa estan moduladas para un armado más sencillo
 void menuPrincipal();
+void nuevaPartida();
+void jugada(int actual, int total);
+
+//Estas funciones setean el color del que va a salir el output. Útil para el diagrama y utilidades varias
+void verde();
+void rojo();
+void amarillo();
+void blanco();
+void negro();
+void colorReset();
 
 int main(){
     menuPrincipal();
+    printf("Saliste del juego. Presione enter para terminar el programa");
+    bufferEnter();
+    bufferEnter();
+    //Fijate que esto lo pongo para mostrarte que es lo que hace el bufferEnter
+    //El tema es que antes andaba de una, ahora lo tengo que poner 2 veces. No sé
     return 0;
 }
 
@@ -23,17 +40,15 @@ void menuPrincipal(){
     printf("\n\n");
     printf("1) Jugar una partida\n");
     printf("2) salir\n");
-    
+    char ingreso[6];
     while(i != (-1)){
         printf("Ingrese un numero para seleccionar: ");
         scanf("%d", &n);
-
         switch(n){
             case 1:
-                //No hay nada hecho jaja
-                limpiarPantalla();
+                nuevaPartida();
                 break;
-            
+
             case 2:
                 return;
                 break;
@@ -48,6 +63,48 @@ void menuPrincipal(){
     }
 }
 
+void nuevaPartida(){
+    int i, j;
+    do{
+        limpiarPantalla();
+        printf("Ingrese una cantidad de partidas para jugar (No mayor a 8): ");
+        scanf("%d", &j); } while(j<1 || j>8);
+        
+    for(i = 0; i < j; i++){
+        jugada(i+1, j);
+    }
+}
+
+void jugada(int actual, int total){
+    printf("Esta es tu jugada %i de %i \n", actual, total);
+}
+
+void verde(){
+    printf("\033[0;32m");
+}
+
+void rojo(){
+    //Setea el color a rojo
+    printf("\033[0;31m");
+}
+
+void amarillo(){
+    printf("\033[0;33m");
+}
+
+void blanco(){
+    printf("\033[0;37m");
+}
+void negro(){
+    printf("\033[1;30m");
+}
+
+void colorReset(){
+    //vuelve al color default
+    printf("\033[0m");
+}
+
+
 void limpiarPantalla(){
   #if defined(linux) || defined(unix) || defined(APPLE)
     system("clear");
@@ -56,18 +113,10 @@ void limpiarPantalla(){
   #if defined(_WIN32) || defined(_WIN64)
     system("cls");
   #endif
-  //Esta funcion la hicimos de esta manera en su momento porque
-  //el comando para limpiar la consola es distinto segun el sistema operativo
-  
-  //Pero además de eso no puede hacerse pidiendote el sistema y despues con un "if"
-  //porque aunque nunca ejecutes esa linea, por estar escrita ya vas a tener un error en el programa
-  
-  //Basicamente, definiendola de esta forma funciona bien y es automatico
  }
 
+
 void bufferEnter(){
-  //fflush(stdin); 
-  //Tengo que revisar esta funcion, anda raro
   int c;
   while ((c = getchar()) != '\n' && c != EOF){} 
 }
