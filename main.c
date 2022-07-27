fee6d18da66696bd43d2efe61aaa169ae82e4b60
Teo Baiocchi <82824948+TeoBaiocchi@users.noreply.github.com>
2022-07-27 17:48:28 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 //Esta funcion simplemente limpia la consola, y detecta automaticamente el 
 //sistema operativo para cambiar el llamado al sistema que lo hace (multiplataforma)
 void limpiarPantalla();
 
-//Funcion "presione enter para continuar" / tambien limpia el buffer 
-//puede ser util en algunos casos de ingreso de strings 
-// (Revisar)
+//Esta funcion espera que presiones enter.
+//Ponerlo como buena practica luego de cada scanf mantiene
+//el buffer limpio y evita problemas de ingresos cuando no queres que pasen
+//Ponerlo una segunda vez con el buffer limpio es, esencialmente, 
+//un "presione enter para continuar"
 void bufferEnter();
 
 //Las secciones del programa estan moduladas para un armado más sencillo
 void menuPrincipal();
 void nuevaPartida();
 void jugada(int actual, int total);
+void mensajeNroJugada(int actual, int total);
 
 //Estas funciones setean el color del que va a salir el output. Útil para el diagrama y utilidades varias
 void verde();
@@ -26,36 +30,44 @@ void colorReset();
 
 int main(){
     menuPrincipal();
-    printf("Saliste del juego. Presione enter para terminar el programa");
+    printf("Saliste del juego. Presione enter para terminar el programa...");
     bufferEnter();
-    bufferEnter();
-    //Fijate que esto lo pongo para mostrarte que es lo que hace el bufferEnter
-    //El tema es que antes andaba de una, ahora lo tengo que poner 2 veces. No sé
     return 0;
 }
 
 void menuPrincipal(){
-    int i = 0, n;
+    int i = 0, n, invalido = 0;
     printf("---WORDLE---\nAUS - Taller de programación I \nBaiocchi Teo, Ceron Santiago");
     printf("\n\n");
     printf("1) Jugar una partida\n");
-    printf("2) salir\n");
+    printf("2) Reglas del juego\n");
+    printf("3) salir\n");
     char ingreso[6];
     while(i != (-1)){
+        if(invalido == 1){
+            printf("El numero ingresado no fue valido. Intente nuevamente... \n");
+        }
         printf("Ingrese un numero para seleccionar: ");
         scanf("%d", &n);
+        bufferEnter();
         switch(n){
             case 1:
+                invalido = 0;
                 nuevaPartida();
                 break;
-
             case 2:
+                invalido = 0;
+                printf("Aca irian las reglas. Hay que acordarse de escribirlas. \n");
+                printf("Presione Enter para continuar");
+                bufferEnter();
+                limpiarPantalla();
+                menuPrincipal();
+            case 3:
                 return;
                 break;
-            
             default:
                 limpiarPantalla();
-                printf("Ingrese una opción valida. \n");
+                invalido = 1;
                 menuPrincipal();
                 break;
         }
@@ -68,7 +80,8 @@ void nuevaPartida(){
     do{
         limpiarPantalla();
         printf("Ingrese una cantidad de partidas para jugar (No mayor a 8): ");
-        scanf("%d", &j); } while(j<1 || j>8);
+        scanf("%d", &j); 
+        bufferEnter(); } while(j<1 || j>8);
         
     for(i = 0; i < j; i++){
         jugada(i+1, j);
@@ -76,9 +89,83 @@ void nuevaPartida(){
 }
 
 void jugada(int actual, int total){
-    printf("Esta es tu jugada %i de %i \n", actual, total);
+    char adivinar[6] = "GATOS"; //palabra de prueba, esto hay que hacer una funcion que traiga una aleatoria de un archivo despues
+    char intento[6];
+    int i, j, k, comprobante = 0;
+    for(i = 0; i < 5; i++){
+        
+        while(comprobante != 1){
+            limpiarPantalla();
+            mensajeNroJugada(actual, total);
+            if(i == 0){
+                char aux[10]; //para testear el largo
+                printf("Ingrese su primer intento: ");
+                scanf("%s", aux);
+                bufferEnter();
+                if(strlen(aux) > 5){
+                    printf("Tu ingreso fue demasiado largo. Recorda que son palabras de 5 caracteres.\n");
+                } else {
+                    if (strlen(aux) < 5) {
+                    printf("Tu ingreso fue demasiado corto. Recorda que son palabras de 5 caracteres.\n");
+                    printf("Presione enter para continuar...");
+                    bufferEnter();
+                    } else {    
+                        strcpy(intento, aux);
+                        comprobante = 1;
+                     }
+                } 
+            } 
+        }
+        
+        //Aca el ingreso ya es valido y guardado en intento
+        comprobante = 0;
+        for(j = 0; j < 5; j++){
+            
+            if(adivinar[j] == intento[j]){
+                printf("El caracter %c esta en la posicion correcta. \n", intento[j]);
+            } else {
+                for(k = 0; k < 5 ; k++){
+                    
+                    if(adivinar[k] == intento[j]){
+                        printf("El caracter %c esta en la palabra, pero en otra posicion. \n", intento[j]);
+                    } else comprobante++;
+                    
+                }
+                
+                printf("comprobante: %i \n", comprobante);
+                if(comprobante == 4){
+                    printf("El caracter %c no estaba en la palabra.", intento[j]);
+                }
+            }
+        }
+    }
 }
 
+
+void getWordInLine(char *fileName, int lineNumber, char *p) {
+    FILE * fp;
+    char * line = NULL;
+    size_t len = 0;
+    ssize_t read;
+
+    fp = fopen(fileName, "r");
+    if (fp == NULL)
+        exit(EXIT_FAILURE);
+
+    int i=1;
+    while ((read = getline(&line, &len, fp)) != -1) {
+        if (i==lineNumber) {
+           printf("%s", line);
+           strcpy(p, line);
+           return;
+        }
+        i++;
+    }
+}
+
+void mensajeNroJugada(int actual, int total){
+    printf("Esta es tu jugada %i de %i \n-----\n", actual, total);
+}
 void verde(){
     printf("\033[0;32m");
 }
